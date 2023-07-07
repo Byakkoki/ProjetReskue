54afbfb0000b1336c9742a4845cb20712f00a0ee
PICANT <jordan404@MBP-de-PICANT.lan>
2023-07-07 15:54:59 +0200

    [FIX] Add SWAGGER on Ticket

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -2,7 +2,9 @@ import {Controller, Get, Param, Post, Put, Req, UseGuards} from "@nestjs/common"
 import {TicketService} from "./ticket.service";
 import {AuthenticatedGuard} from "../commons/security/guard/authenticated.guard";
 import RequestWithUser from "../commons/request.user";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { TicketEntity } from "./ticket.entity";
+import { User } from "src/user/user.entity";
 
 @ApiTags('Ticket')
 @Controller('ticket/event/:idEvent')
@@ -12,23 +14,71 @@ export class TicketController {
         private ticketService: TicketService
     ) { }
 
+    @ApiOperation({
+        description: `This route received all ticket`,
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'You received all the user',
+        type: [TicketEntity],
+    })
+    @ApiResponse({
+        status: 401,
+        description: 'You need to be identified with your token',
+    })
     @UseGuards(AuthenticatedGuard)
     @Get()
     getAllTickets(@Req() request: RequestWithUser) {
         return this.ticketService.getAllTickets(request.user)
     }
 
+    @ApiOperation({
+        description: `This route received all available ticket of one event`,
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'You received all the ticket available',
+        type: [TicketEntity],
+    })
+    @ApiResponse({
+        status: 401,
+        description: 'You need to be identified with your token',
+    })
     @Get('available')
     getAllTicketAvailableByOneEvent(@Param('idEvent') idEvent: string) {
         return this.ticketService.getAllTicketAvailableByOneEvent(idEvent)
     }
 
+    @ApiOperation({
+        description: `This route received a ticket with booked`,
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'You received one ticket',
+        type: [TicketEntity],
+    })
+    @ApiResponse({
+        status: 401,
+        description: 'You need to be identified with your token',
+    })
     @UseGuards(AuthenticatedGuard)
     @Post('book')
     bookOneTicket(@Req() request: RequestWithUser, @Param('idEvent') id: string) {
         return this.ticketService.bookOneTicket(id, request.user)
     }
 
+    @ApiOperation({
+        description: `This route received a ticket with unbooked`,
+    })
+    @ApiResponse({
+        status: 200,
+        description: 'You received one ticket',
+        type: [TicketEntity],
+    })
+    @ApiResponse({
+        status: 401,
+        description: 'You need to be identified with your token',
+    })
     @UseGuards(AuthenticatedGuard)
     @Put('remove-book/:idTicket')
     removeOneBookForOneUser(@Param('idTicket') idTicket: string, @Param('idEvent') idEvent: string, @Req() request: RequestWithUser) {
--- src/ticket/ticket.entity.ts
+++ src/ticket/ticket.entity.ts
@@ -1,15 +1,32 @@
 import {Cascade, Entity, ManyToOne, PrimaryKey} from "@mikro-orm/core";
 import {User} from "../user/user.entity";
 import {EventEntity} from "../event/event.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity()
 export class TicketEntity {
+
+    @ApiProperty({
+        name: 'id',
+        description: 'The id of the Ticker',
+        example: '20ee542c-4416-49f9-ae99-d4106de3a018'
+    })
     @PrimaryKey()
     id: string
 
+    @ApiProperty({
+        name: 'id',
+        description: 'The id of the user',
+        example: '20ee542c-4416-49f9-ae99-d4106de3a018'
+    })
     @ManyToOne(() => User, { nullable: true, cascade: [Cascade.ALL] })
     owner: User
 
+    @ApiProperty({
+        name: 'id',
+        description: 'The id of the event',
+        example: '20ee542c-4416-49f9-ae99-d4106de3a018'
+    })
     @ManyToOne(() => EventEntity, { cascade: [ Cascade.ALL] })
     event: EventEntity
 }
